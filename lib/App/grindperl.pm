1857a8eeacb9c1e54ddf5cb353ad0ca007d2462d
David Golden <dagolden@cpan.org>
2011-07-21 11:59:22 -0400

    initial import of App::grindperl

---
+++ lib/App/grindperl.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package App::grindperl;
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+# ABSTRACT: No abstract given for App::grindperl
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use App::grindperl;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
+# vim: ts=2 sts=2 sw=2 et:
